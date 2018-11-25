3f50696347651aa6f31f19ba02c537d5f28fb55b
HappyFakeBoulder <40407420+HappyFakeBoulder@users.noreply.github.com>
2018-11-25 18:02:31 -0500

    Update setup.py

--- hfble/setup.py
+++ hfble/setup.py
@@ -1,1 +1,2 @@
-print("more things")
+f = open("README.txt", "r")
+__import__("distutils").core.setup(name="hfble", version="2.0", packages=["hfble"], license="MIT License", long_description=(f.read(), f.close())[0])
